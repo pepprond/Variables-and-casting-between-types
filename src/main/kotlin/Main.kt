17b61871069d87d60b1edd10b0a95a9ae3b65fb2
pepprond <rondelli95@gmail.com>
2022-11-22 16:23:29 +0100

    final commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+   val number1: Double = 15.780
+   val number2: Long = 3566
+    val number3: Short = 104
+    val sum =number1 + number2 + number3
+
+    println("the result of the sum is: ${sum}")
+
+}
